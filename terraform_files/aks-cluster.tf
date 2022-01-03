c8dce30edd514574821d6ef5a696d04fc061c76c
parsh <parshchakurkar@gmail.com>
2022-01-03 10:52:48 -0700

    previous commit not consider cluster.tf

--- terraform_files/aks-cluster.tf
+++ terraform_files/aks-cluster.tf
@@ -1,7 +1,3 @@
-provider "azurerm" {
-  features{}
-}
-
 resource "azurerm_kubernetes_cluster" "single-app-cluster" {
   resource_group_name = azurerm_resource_group.single-app-resource.name
   location            = azurerm_resource_group.single-app-resource.location
